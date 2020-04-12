5d6830aedb0b33d5d8cfc6d2fc7a8cbde00e4345
adidner <adidner@gmail.com>
2020-04-11 19:08:52 -0700

    added sending email functionality

--- script.py
+++ script.py
@@ -1,9 +1,27 @@
 from openpyxl import load_workbook
+import smtplib, ssl
 
 failingGirls = []
 gpaMinimum = 2.0
 nameGPAFile = "Base1.xlsx"
 nameEmailFile = "Contact Sheet Format.xlsx"
+
+yourEmail = ""
+password = ""
+
+port = 465
+message = """\
+Subject: Help with Grades
+
+This is the Scholarship Chair for (Insert Sorority Name Here).
+In accordance with our bylaws I've been asked to reach out to you because your grades have begun to slip a bit
+I'd like to schedule a 1 on 1 to disscuss new tactics and anything I/the soririty can do to help you
+
+Thanks,
+Name
+"""
+
+context = ssl.create_default_context()
 
 
 def grabDataFromGPATable(targetfilename):
@@ -15,8 +33,6 @@ def grabDataFromGPATable(targetfilename):
     firstNames = sheet["A"]
     lastNames = sheet["B"]
     GPAs = sheet["E"]
-
-
 
 
 
@@ -34,7 +50,7 @@ def grabDataFromGPATable(targetfilename):
     print(failingGirls)
 
 
-function grabDataFromEmailTable(targetfilename):
+def grabDataFromEmailTable(targetfilename):
 
     workbook2 = load_workbook(filename=targetfilename)
 
@@ -45,20 +61,32 @@ function grabDataFromEmailTable(targetfilename):
     lastNames2 = sheet2["C"]
     emails = sheet2["G"]
 
+    print("Go here with your Gmail logged in and allow apps like this one to login as you: https://www.google.com/settings/security/lesssecureapps")
+
+    yourEmail = input("Enter Your yourEmail: ")
+    password = input("Enter your password: ")
 
     for currentGirl in failingGirls:
         for first, last, email in zip(firstNames2, lastNames2, emails):
             if currentGirl["first"] == first.value.lower() and currentGirl["last"] == last.value.lower():
                 currentGirl["email"] = email.value
-                sendEmail(currentGirl)
+                sendEmail(currentGirl, yourEmail, password)
 
 
-function sendEmail(currentGirl):
-    print("NOT ACTUALLY SENDING EMAILS YET, still work to do here")
+def sendEmail(currentGirl, yourEmail, password):
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(yourEmail, password)
+        server.sendmail(yourEmail, yourEmail, message)
+
+
+
+
 
 
 
 def main():
+
     grabDataFromGPATable(nameGPAFile)
     grabDataFromEmailTable(nameEmailFile)
 
